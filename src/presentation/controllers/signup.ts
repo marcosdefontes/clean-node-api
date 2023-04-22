f10452387574c97bbc689a5a225e5cf46e0aa5cc
Marcos Freire <marcosdefontes@gmail.com>
2023-04-22 20:54:50 -0300

    Refactor

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,11 +4,10 @@ import { HttpRequest, HttpResponse } from "./protocols/http";
 
 export class SignUpController {
   handle(httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError("name"));
-    }
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError("email"));
-    }
+    const requiredFiels = ["name", "email"];
+    for (const field of requiredFiels)
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field));
+      }
   }
 }
