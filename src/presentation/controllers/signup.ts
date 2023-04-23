05c14ca19af8b8f9c47760cd2e4c760cb6fbd9f0
Marcos Freire <marcosdefontes@gmail.com>
2023-04-22 21:04:09 -0300

    Controller

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,9 @@
 import { badRequest } from "../helpers/http-helpers";
 import { MissingParamError } from "./errors/missing-param-error";
+import { Controller } from "./protocols/controller";
 import { HttpRequest, HttpResponse } from "./protocols/http";
 
-export class SignUpController {
+export class SignUpController implements Controller {
   handle(httpRequest: HttpRequest): HttpResponse {
     const requiredFiels = ["name", "email", "password", "passwordConfirmation"];
     for (const field of requiredFiels)
