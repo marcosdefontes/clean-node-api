29be74251a06320b834d52aa583998042a01aa8a
Marcos Freire <marcosdefontes@gmail.com>
2023-04-23 18:31:28 -0300

    Server Error

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,6 +1,7 @@
 import { badRequest } from "../helpers/http-helpers";
 import { InvalidParamError } from "./errors/invalid-param-error";
 import { MissingParamError } from "./errors/missing-param-error";
+import { ServerError } from "./errors/server-error";
 import { Controller } from "./protocols/controller";
 import { EmailValidator } from "./protocols/email-validator";
 import { HttpRequest, HttpResponse } from "./protocols/http";
@@ -12,13 +13,20 @@ export class SignUpController implements Controller {
   }
   handle(httpRequest: HttpRequest): HttpResponse {
     const requiredFiels = ["name", "email", "password", "passwordConfirmation"];
-    for (const field of requiredFiels)
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field));
+    try {
+      for (const field of requiredFiels)
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field));
+        }
+      const isValid = this.emailValidator.isValid(httpRequest.body.email);
+      if (!isValid) {
+        return badRequest(new InvalidParamError("email"));
       }
-    const isValid = this.emailValidator.isValid(httpRequest.body.email);
-    if (!isValid) {
-      return badRequest(new InvalidParamError("email"));
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError(),
+      };
     }
   }
 }
