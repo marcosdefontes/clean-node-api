bc387c1cd7b5b4b1734e180671e4e3f90ed0a352
Marcos Freire <marcosdefontes@gmail.com>
2023-05-12 22:30:07 -0300

    makeSut

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,4 +1,4 @@
-import { EmvailValidatorAdapter } from "./email-validator";
+import { EmailValidatorAdapter } from "./email-validator";
 import validator from "validator";
 
 jest.mock("validator", () => ({
@@ -7,20 +7,23 @@ jest.mock("validator", () => ({
   },
 }));
 
+const makeSut = (): EmailValidatorAdapter => {
+  return new EmailValidatorAdapter();
+};
 describe("EmailValidator Adapter", () => {
   test("Should return false if validator return false", () => {
-    const sut = new EmvailValidatorAdapter();
+    const sut = makeSut();
     jest.spyOn(validator, "isEmail").mockReturnValueOnce(false);
     const isValid = sut.isValid("invalid_email@gmail.com");
     expect(isValid).toBe(false);
   });
   test("Should return true if validator return true", () => {
-    const sut = new EmvailValidatorAdapter();
+    const sut = makeSut();
     const isValid = sut.isValid("valid_email@mail.com");
     expect(isValid).toBe(true);
   });
   test("Should call validator if correct email", () => {
-    const sut = new EmvailValidatorAdapter();
+    const sut = makeSut();
     const isEmailSpy = jest.spyOn(validator, "isEmail");
     const email = "any_email@mail.com";
     sut.isValid(email);
--- src/utils/email-validator.ts
+++ src/utils/email-validator.ts
@@ -1,7 +1,7 @@
 import { EmailValidator } from "../presentation/protocols/email-validator";
 import validator from "validator";
 
-export class EmvailValidatorAdapter implements EmailValidator {
+export class EmailValidatorAdapter implements EmailValidator {
   isValid(email: string): boolean {
     return validator.isEmail(email);
   }
