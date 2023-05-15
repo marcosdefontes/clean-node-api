5b26466a024ce3bc537fd16f241a8d8a6e8fa3d7
Marcos Freire <marcosdefontes@gmail.com>
2023-05-14 21:25:14 -0300

    Should return a hash on success

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import bcrypt from "bcrypt";
 import { BcryptAdapter } from "./bcrypt-adapter";
 
+jest.mock("bcrypt", () => ({
+  async hash(): Promise<string> {
+    return new Promise((resolve) => resolve("hash"));
+  },
+}));
+
 describe("Bcrypt Adapter", () => {
   test("Should call bcrypt with correct value", async () => {
     const salt = 12;
@@ -9,5 +15,12 @@ describe("Bcrypt Adapter", () => {
     const val = "any_value";
     await sut.encrypt(val);
     expect(hashSpy).toHaveBeenCalledWith(val, salt);
+  });
+  test("Should return a hash on success", async () => {
+    const salt = 12;
+    const sut = new BcryptAdapter(salt);
+    const val = "any_value";
+    const hash = await sut.encrypt(val);
+    expect(hash).toBe("hash");
   });
 });
