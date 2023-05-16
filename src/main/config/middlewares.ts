8c34e715b20d697ca6c8c0c612429770cc14142d
Marcos Freire <marcosdefontes@gmail.com>
2023-05-16 18:53:42 -0300

    CORS tests

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -3,5 +3,5 @@ import { bodyParser } from "../middlewares/body-parser";
 import { cors } from "../middlewares/cors";
 export default (app: Express): void => {
   app.use(bodyParser);
-  app.use(cors);
+  app.use(cors); 
 };
