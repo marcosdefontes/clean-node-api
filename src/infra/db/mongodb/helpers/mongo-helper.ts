a7c58c545292a796f26c9f1629072d33e457339f
Marcos Freire <marcosdefontes@gmail.com>
2023-05-19 10:17:41 -0300

    Reconectar mongodb

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,15 +1,20 @@
 import { MongoClient, Collection } from "mongodb";
 
 export const MongoHelper = {
+  uri: null as string,
   client: null as MongoClient,
   async connect(uri: string): Promise<void> {
+    this.uri = uri;
     this.client = await MongoClient.connect(uri);
   },
   async disconnect(): Promise<void> {
-    await this.client.close();
+    await this.client?.close();
+    this.client = null;
   },
 
-  getCollection(name: string): Collection {
+  async getCollection(name: string): Promise<Collection> {
+    if (!this.client?.isConnected()) await this.connect(this.uri);
+
     return this.client.db().collection(name);
   },
 
