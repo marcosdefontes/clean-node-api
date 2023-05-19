b1a65686e5ac71afc0e143da011ca93e27faeca4
Marcos Freire <marcosdefontes@gmail.com>
2023-05-19 16:59:34 -0300

    Refactor log

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -2,13 +2,19 @@ import { DbAddAccount } from "../../data/usecases/add-account/db-add-account";
 import { BcryptAdapter } from "../../infra/cryptography/bcrypt-adapter";
 import { AccountMongoRepository } from "../../infra/db/mongodb/account-repository/account";
 import { SignUpController } from "../../presentation/controllers/signup/signup";
+import { Controller } from "../../presentation/protocols";
 import { EmailValidatorAdapter } from "../../utils/email-validator-adapter";
+import { LogControllerDecorator } from "../decorators/log";
 
-export const makeSignupController = (): SignUpController => {
+export const makeSignupController = (): Controller => {
   const emailValidatorAdapter = new EmailValidatorAdapter();
   const salt = 12;
   const encrypter = new BcryptAdapter(salt);
   const addAccountRepo = new AccountMongoRepository();
   const addAccount = new DbAddAccount(encrypter, addAccountRepo);
-  return new SignUpController(emailValidatorAdapter, addAccount);
+  const signUpController = new SignUpController(
+    emailValidatorAdapter,
+    addAccount
+  );
+  return new LogControllerDecorator(signUpController);
 };
