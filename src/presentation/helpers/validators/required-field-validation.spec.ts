09530918d95675038d0dd2f1949bbe859b5bf82d
Marcos Freire <marcosdefontes@gmail.com>
2023-05-23 16:32:59 -0300

    Refactor

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -1,14 +1,16 @@
 import { MissingParamError } from "../../errors";
 import { RequiredFieldValidation } from "./required-field-validation";
 
+const makeSut = (): RequiredFieldValidation =>
+  new RequiredFieldValidation("field");
 describe("Required Field Validation", () => {
   test("should return a MissingParamError if validation fails", () => {
-    const sut = new RequiredFieldValidation("field");
+    const sut = makeSut();
     const error = sut.validate({ otherField: "any" });
     expect(error).toEqual(new MissingParamError("field"));
   });
   test("should return null if validation fails", () => {
-    const sut = new RequiredFieldValidation("field");
+    const sut = makeSut();
     const error = sut.validate({ field: "any" });
     expect(error).toBeFalsy();
   });
