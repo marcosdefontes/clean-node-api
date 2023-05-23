5b7d04baaec1f17f3b86cea3ad9b14c51f1dbc41
Marcos Freire <marcosdefontes@gmail.com>
2023-05-23 16:31:15 -0300

    should return null if validation fails

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -7,4 +7,9 @@ describe("Required Field Validation", () => {
     const error = sut.validate({ otherField: "any" });
     expect(error).toEqual(new MissingParamError("field"));
   });
+  test("should return null if validation fails", () => {
+    const sut = new RequiredFieldValidation("field");
+    const error = sut.validate({ field: "any" });
+    expect(error).toBeFalsy();
+  });
 });
