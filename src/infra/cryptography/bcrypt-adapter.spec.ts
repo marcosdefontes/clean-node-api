2c43d7955fccb22e31624c5ea2d4c057eaea5ad4
Marcos Freire <marcosdefontes@gmail.com>
2023-05-24 18:58:04 -0300

    Should return true if compare succeeds

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -49,4 +49,10 @@ describe("Bcrypt Adapter", () => {
     await sut.compare("any_value", "any_hash");
     expect(compareSpy).toHaveBeenCalledWith("any_value", "any_hash");
   });
+
+  test("Should return true if compare succeeds", async () => {
+    const sut = makeSut();
+    const compare = await sut.compare("any_value", "any_hash");
+    expect(compare).toBeTruthy();
+  });
 });
