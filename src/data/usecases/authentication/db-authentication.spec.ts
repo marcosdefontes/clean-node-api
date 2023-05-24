736ee8b53b48f88739dba2ad3a7cfa2f412108b9
Marcos Freire <marcosdefontes@gmail.com>
2023-05-24 16:46:51 -0300

    Should throw if TokenGenerator throws

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -125,4 +125,15 @@ describe("DB Authentication", () => {
     await sut.auth(makeFakeAccount());
     expect(generateSpy).toHaveBeenCalledWith("any_id");
   });
+
+  test("Should throw if TokenGenerator throws", () => {
+    const { sut, tokenGeneratorStub } = makeSut();
+    jest
+      .spyOn(tokenGeneratorStub, "generate")
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      );
+    const promise = sut.auth(makeFakeAuthentication());
+    expect(promise).rejects.toThrow();
+  });
 });
