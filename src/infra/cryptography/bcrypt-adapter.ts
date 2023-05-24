e702c1ccbbea863e036cd172b5b8e8d6309c1070
Marcos Freire <marcosdefontes@gmail.com>
2023-05-24 18:55:04 -0300

    Should call compare with correct values

--- src/infra/cryptography/bcrypt-adapter.ts
+++ src/infra/cryptography/bcrypt-adapter.ts
@@ -1,11 +1,16 @@
 import bcrypt from "bcrypt";
 import { Hasher } from "../../data/protocols/cryptography/hasher";
+import { HashComparer } from "../../data/protocols/cryptography/hash-comparer";
 
-export class BcryptAdapter implements Hasher {
+export class BcryptAdapter implements Hasher, HashComparer {
   private readonly salt: number;
 
   constructor(salt: number) {
     this.salt = salt;
+  }
+  async compare(value: string, hash: string): Promise<boolean> {
+    await bcrypt.compare(value, hash);
+    return new Promise((resolve) => resolve(true));
   }
 
   async hash(value: string): Promise<string> {
