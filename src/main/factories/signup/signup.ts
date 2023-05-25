ef82940c66c5e4f4fa8528147ac38cc23ad02756
Marcos Freire <marcosdefontes@gmail.com>
2023-05-25 08:21:44 -0300

    Account repository folder

--- src/main/factories/signup/signup.ts
+++ src/main/factories/signup/signup.ts
@@ -1,6 +1,6 @@
 import { DbAddAccount } from "../../../data/usecases/add-account/db-add-account";
 import { BcryptAdapter } from "../../../infra/cryptography/bcrypt-adapter/bcrypt-adapter";
-import { AccountMongoRepository } from "../../../infra/db/mongodb/account-repository/account";
+import { AccountMongoRepository } from "../../../infra/db/mongodb/account/account-repository";
 import { LogMongoRepository } from "../../../infra/db/mongodb/log-repository/log";
 import { SignUpController } from "../../../presentation/controllers/signup/signup";
 import { Controller } from "../../../presentation/protocols";
