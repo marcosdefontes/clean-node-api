08dcca88528e443cda424bc8d5fe62eff9a3eee0
Marcos Freire <marcosdefontes@gmail.com>
2023-05-25 21:27:21 -0300

    Update npm scritps

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -3,7 +3,10 @@ import fg from "fast-glob";
 export default (app: Express): void => {
   const router = Router();
   app.use("/api", router);
-  fg.sync("**/main/routes/**routes.ts").map(async (file) => {
-    (await import(`../../../${file}`)).default(router);
-  });
+  // fg.sync("**/main/routes/**routes.ts").map(async (file) => {
+  //   (await import(`../../../${file}`)).default(router);
+  // });
+  fg.sync(`${__dirname}/../routes/**routes.??`).map(async (file) =>
+    (await import(file)).default(router)
+  );
 };
